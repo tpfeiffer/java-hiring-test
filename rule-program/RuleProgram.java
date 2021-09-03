6d29ea2e088ca212b9358a77722b1d0376e61976
Daniel Gondos <daniel.gondos@amadeus.com>
2021-09-03 14:13:03 +1000

    Making RuleProgram test more fair

--- rule-program/RuleProgram.java
+++ rule-program/RuleProgram.java
@@ -1,5 +1,3 @@
-
-
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
@@ -7,12 +5,10 @@ import java.util.Observer;
 public class RuleProgram implements Observer {
 
     private Program program;
-    private Rules businessRules;
     private ReferenceData referenceData;
 
-    public RuleProgram(Program program, Rules businessRules, ReferenceData referenceData) {
+    public RuleProgram(Program program, ReferenceData referenceData) {
             this.program = program;
-            this.businessRules = businessRules;
             this.referenceData = referenceData;
             program.addObserver(this);
             referenceData.addObserver(this);
@@ -23,10 +19,10 @@ public class RuleProgram implements Observer {
 
         if (program.equals(o)) {
 
-            Change change = businessRules.computeChange();
+            Change change = BusinessRules.computeChange();
 
             // FIXME We are risking an infinite loop here...
-            change.apply(program);
+            program.applyChange(change);
 
 
         } else if (o instanceof ReferenceData) {
