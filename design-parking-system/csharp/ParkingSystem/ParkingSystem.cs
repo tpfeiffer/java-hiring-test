13d7b79bc8f8f43c877ad4d27aad16fa27bbbfae
tpfeiffer <thilo.pfeiffer@amadeus.com>
2023-07-17 15:07:37 +1000

    add csharp test

---
+++ design-parking-system/csharp/ParkingSystem/ParkingSystem.cs
@@ -0,0 +1,36 @@
+﻿// Hello World! program
+namespace ParkingSystem
+{
+    class ParkingSystem
+    {
+        private int big;
+        private int medium;
+        private int small;
+
+        public ParkingSystem(int big, int medium, int small)
+        {
+            this.big = big;
+            this.medium = medium;
+            this.small = small;
+        }
+
+        private bool AddCar(int v)
+        {
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+
+            ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+
+            System.Console.WriteLine("Pass test 1: " + (parkingSystem.AddCar(1) == true));
+            System.Console.WriteLine("Pass test 2: " + (parkingSystem.AddCar(2) == true));
+            System.Console.WriteLine("Pass test 3: " + (parkingSystem.AddCar(3) == false));
+            System.Console.WriteLine("Pass test 4: " + (parkingSystem.AddCar(1) == false));
+
+        }
+
+
+    }
+}
